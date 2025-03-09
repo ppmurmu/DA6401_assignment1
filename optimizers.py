bd054ab0877a1357acccfd292d9b11864605e0ae
ppmurmu <pradeeppetermurmu@gmail.com>
2025-03-10 01:08:14 +0530

    add NAG

--- optimizers.py
+++ optimizers.py
@@ -29,7 +29,7 @@ class MomentumGD:
         self.momentum = momentum
         self.decay = decay
 
-        # Initialize momentum terms with shapes of  weights and biases
+        # Initialize momentum terms
         self.h_weights = [np.zeros_like(w) for w in self.nn.weights]
         self.h_biases = [np.zeros_like(b) for b in self.nn.biases]
 
@@ -45,5 +45,29 @@ class MomentumGD:
             self.nn.weights[i] -= learning_rate * (self.h_weights[i] + weight_decay * self.nn.weights[i])
             self.nn.biases[i] -= learning_rate * (self.h_biases[i] + weight_decay * self.nn.biases[i])
 
+
+#-------- Nesterov Accelerated Gradient Descent  ------------------
+class NAG:
+    def __init__(self, nn, lr=0.001, momentum=0.9, decay=0.0):
+        self.nn = nn
+        self.lr = lr
+        self.momentum = momentum
+        self.decay = decay
+
+        # Initialize momentum terms
+        self.h_weights = [np.zeros_like(w) for w in self.nn.weights]
+        self.h_biases = [np.zeros_like(b) for b in self.nn.biases]
+
+    def step(self, d_weights, d_biases):
+        learning_rate = self.lr
+        weight_decay = self.decay  
+        for i in range(self.nn.hidden_layers + 1):
+            # Compute Nesterov momentum update
+            self.h_weights[i] = self.momentum * self.h_weights[i] + d_weights[i]
+            self.h_biases[i] = self.momentum * self.h_biases[i] + d_biases[i]
+
+            # Update weights and biases with NAG formula
+            self.nn.weights[i] -= learning_rate * (self.momentum * self.h_weights[i] + d_weights[i] + weight_decay * self.nn.weights[i])
+            self.nn.biases[i] -= learning_rate * (self.momentum * self.h_biases[i] + d_biases[i] + weight_decay * self.nn.biases[i])
 
 #------- ADD OPTIMIZER CLASSES HERE--------------------
