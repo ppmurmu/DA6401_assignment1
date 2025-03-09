2047fa396ad5c0d270545ac51c3b04fc29defd24
ppmurmu <pradeeppetermurmu@gmail.com>
2025-03-09 22:37:37 +0530

    add momentum gd

--- optimizers.py
+++ optimizers.py
@@ -1,10 +1,7 @@
-#--- this file contains all the optimizer classes-------
 import numpy as np
 
-#nn is the neural network instance we pass.
-
-
 #-------- Stochastic Gradient Descent ------------------
+#nn is the neural network instance we pass.
 class SGD:
     def __init__(self, nn, lr=0.001, decay=0.0):
         self.nn = nn
@@ -23,6 +20,30 @@ class SGD:
             # Update weights and biases
             self.nn.weights[layer] -= weight_update
             self.nn.biases[layer] -= bias_update
+
+
+class MomentumGD:
+    def __init__(self, nn, lr=0.001, momentum=0.9, decay=0.0):
+        self.nn = nn
+        self.lr = lr
+        self.momentum = momentum
+        self.decay = decay
+
+        # Initialize momentum terms with shapes of  weights and biases
+        self.h_weights = [np.zeros_like(w) for w in self.nn.weights]
+        self.h_biases = [np.zeros_like(b) for b in self.nn.biases]
+
+    def step(self, d_weights, d_biases):
+        learning_rate = self.lr
+        weight_decay = self.decay  
+        for i in range(self.nn.hidden_layers + 1):
+            # Compute momentum update
+            self.h_weights[i] = self.momentum * self.h_weights[i] + d_weights[i]
+            self.h_biases[i] = self.momentum * self.h_biases[i] + d_biases[i]
+
+            # Update weights and biases
+            self.nn.weights[i] -= learning_rate * (self.h_weights[i] + weight_decay * self.nn.weights[i])
+            self.nn.biases[i] -= learning_rate * (self.h_biases[i] + weight_decay * self.nn.biases[i])
 
 
 #------- ADD OPTIMIZER CLASSES HERE--------------------
