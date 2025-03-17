473a3c3b43d2d614b62b87ae18dc31a795a68cd3
ppmurmu <pradeeppetermurmu@gmail.com>
2025-03-17 17:16:50 +0530

    preprocess and load data func

--- wandb_sweep.py
+++ wandb_sweep.py
@@ -10,3 +10,27 @@ def loss(loss, y, y_pred):
         return np.sum((y - y_pred) ** 2) / 2
     else:
         raise Exception("Invalid loss function")
+    
+    
+#-----pre processing of data-----------
+def preprocess(x, y):
+        x = x.reshape(x.shape[0], 784) / 255  # Flatten & normalize
+        y = np.eye(10)[y]  # One-hot encode labels
+        return x, y
+
+#---function to load data--------------
+def load_data(type, dataset='fashion_mnist'):
+
+    x, y, x_test, y_test = (), (), (), ()
+    
+    if dataset == 'mnist':
+        (x, y), (x_test, y_test) = mnist.load_data()
+    elif dataset == 'fashion_mnist':
+        (x, y), (x_test, y_test) = fashion_mnist.load_data()
+
+    if type == 'train':
+        return preprocess(x, y)
+    elif type == 'test':
+        return preprocess(x_test, y_test)
+    
+
